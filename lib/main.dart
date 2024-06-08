177c230b268a3186605ab835ff9ad01624f39d5c
devmak <khan@nochallenge.net>
2024-06-09 00:10:39 +0600

    button styling

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,12 +17,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData.dark(),
+      theme: ThemeData.dark(
+        useMaterial3: true,
+      ),
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: const Color.fromARGB(255, 65, 63, 68),
           centerTitle: true,
-          title: Text('RB Bank App'),
+          title: const Text('RB Bank App'),
         ),
         body: Container(
           padding: EdgeInsets.all(20),
@@ -30,10 +34,28 @@ class MyApp extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text("Balance"),
-              ElevatedButton(
-                onPressed: aFunction,
-                child: Text("Click"),
+              Expanded(
+                flex: 9,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Balance"),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text("1"),
+                  ],
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: ElevatedButton(
+                  onPressed: aFunction,
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.amber[700],
+                      minimumSize: Size(double.infinity, 0)),
+                  child: Text("Add Balance"),
+                ),
               ),
             ],
           ),
